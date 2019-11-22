284080da88661c7aad9b74eb4a86a94ec3ed3c6c
kayleyhenderson <58082284+kayleyhenderson@users.noreply.github.com>
2019-11-22 10:46:29 -0500

    Add files via upload

---
+++ Classwork.py
@@ -0,0 +1,16 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: /Users/kayleyhenderson/Documents/KHendersonCW4.py =========
+How many people might show up? 25
+>>> 
+========= RESTART: /Users/kayleyhenderson/Documents/KHendersonCW4.py =========
+How many people might show up? 3
+Welcome to my program for Thanksgiving!
+We need 44 Turkey
+We need 44 Apple pie
+We need 44 Mashed potatoes
+We need 44 Mac and cheese
+Do you want to keep going? Type y to exity
+>>> 
